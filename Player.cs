4965e31c85c09ad95ce1a1ab9bff3159d16dade4
SamPM092 <sammich4294@gmail.com>
2025-05-19 15:54:45 +0700

    level up function

--- Player.cs
+++ Player.cs
@@ -13,11 +13,12 @@ namespace MyApp
         public int maxHealth { get; set; } = 10;
         public int health { get; set; } = 10;
         public int attackValue { get; set; } = 1;
-        public int defenseValue { get; set; } = 0 ;
+        public int defenseValue { get; set; } = 0;
         public int weaponValue { get; set; } = 1;
         public int potion { get; set; } = 3;
-
         public int mods { get; set; } = 0; //for modifying power level
+        public int level { get; set; } = 1;
+        public int exp { get; set; } = 0;
 
         public enum PLayerClass { Warrior, Knight, Alchemist }; //3 class for now
         public PLayerClass currentClass { get; set; } = PLayerClass.Warrior; //default class
@@ -41,6 +42,34 @@ namespace MyApp
             int upper = 15 * mods + 50;
             int lower = 10 * mods + 10;
             return rand.Next(lower, upper);
+        }
+
+        public int GetXP() //randomize money dropped
+        {
+            int upper = 25 * mods + 45;
+            int lower = 10 * mods + 15;
+            return rand.Next(lower, upper);
+        }
+
+        public int LevelUpValue()
+        {
+            return 50 * level + 400;
+        }
+
+        public bool CanLevelUp()
+        {
+            if (exp >= LevelUpValue()) return true;
+            else return false;
+        }
+
+        public void LevelUp()
+        {
+            while (CanLevelUp())
+            {
+                exp -= LevelUpValue();
+                level++;
+            }
+            Program.Print("Kamu naik level menjadi level " + level + " !");
         }
     }
 }
